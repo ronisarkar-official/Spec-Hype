b135d8544f153042ce66cf2531af54d58483e00a
Roni Sarkar <ronisarkar742202@gmail.com>
2025-05-04 14:03:14 +0530

    image link

--- main.js
+++ main.js
@@ -9,15 +9,19 @@ function renderPosts(start, end) {
   const visiblePosts = posts.slice(start, end);
   visiblePosts.forEach((post) => {
     const postHTML = `
-      <article  class="bg-white rounded-lg shadow p-4 flex flex-col sm:flex-row gap-4 ">
-      <div class="overflow-hidden rounded-lg ">
-    <img src="${post.image}" alt="Thumbnail" class="w-full h-fit sm:w-60 rounded-md hover:scale-110 transition-transform duration-300" /></div>
-    <div>
-      <h2 class="text-lg font-semibold text-blue-700 hover:underline">
-        <a href="${post.link}">${post.title}</a>
-      </h2>
-      <p class="text-sm text-gray-600  ">${post.description}</p>
-    </div>
+      <article class="bg-white rounded-lg shadow p-4 flex flex-col sm:flex-row gap-4">
+  <div class="overflow-hidden rounded-lg">
+    <a href="${post.link}">
+      <img src="${post.image}" alt="Thumbnail"
+        class="w-full h-[136px] sm:w-60 rounded-md hover:scale-110 transition-transform duration-300" />
+    </a>
+  </div>
+  <div>
+    <h2 class="text-lg font-semibold text-blue-700 hover:underline">
+      <a href="${post.link}">${post.title}</a>
+    </h2>
+    <p class="text-sm text-gray-600">${post.description}</p>
+  </div>
 </article>
 
 
