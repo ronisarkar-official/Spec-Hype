4976adcf46f4bbf595fe545ab0ae170fe74b43c3
Roni Sarkar <ronisarkar742202@gmail.com>
2025-05-23 14:57:58 +0530

    minor fix

--- js/homePost.js
+++ js/homePost.js
@@ -46,7 +46,7 @@ export const posts = [
 		description:
 			'Cricket fans, gear up! Cricket 24 delivers stunning visuals and immersive gameplay',
 		image:
-			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiok3RIHSWyQd3rFlXC5-PG3lluq9KwvSyqKr0q_sStZrc_aQ6088wt1P6p-sO_3CGwfMxJYowJa7U_nIn_tmUkudzDgDxTsmuIX6CtH8lDk-4Z06Yce5PLarjN6HADbg8_mmI9iLmIugWPlHaOXArvk-fRDnhtaGTKEOfXgyp8HaP3ePSb594njGBzfcOF/w640-h360/Cricket%2024%20PC%20System%20Requirements.webp',
+			'https://ik.imagekit.io/2zeqzsn1n/Cricket-24-PC-System-Requirements.webp?f-webp,q-auto',
 		link: 'https://www.spechype.com/games/cricket-24-pc-requirements-2025.html',
 	},
 
@@ -54,7 +54,7 @@ export const posts = [
 		title: 'Hyper Light Breaker System Requirements - Can I Run It?',
 		description: 'Hyper Light Breaker" is an upcoming roguelite action game.',
 		image:
-			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgaWS9FhciBjTyzDEE2zOs2_inEbk5EuD1EYrZmanqyiOrWblFSA0WixGH1aeWfz_O1TOJMhWDi5uRvGj9PeUkPoOuj0bt5u6HxGbMOSTAEp0URY9l1Y_eQpc5ZhUOXP2uQYpVR8Vvc-npq7YbiPZY0lofuXq-P6cSE8-Gk4nBK_valfeqHNxpSR5S-YlF6/w640-h360/Hyper%20Light%20Breaker%20System%20Requirements%20.png',
+			'https://ik.imagekit.io/2zeqzsn1n/Hyper-Light-Breaker-System-Requirements.webp?f-webp,q-auto',
 		link: 'https://www.spechype.com/games/hyper-light-breaker-requirements.html',
 	},
 	{
@@ -62,7 +62,7 @@ export const posts = [
 		description:
 			'Check the full PC system requirements for Elden Ring: Nightreign, including minimum and recommended specs, 4K gaming builds, storage tips, and co-op settings.',
 		image:
-			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgn6xCE7y45BPdP2mX3hLqRfg5jgJu5FGdOBx2yzC0Meoznlv_4hRANvVrvm7tsqdDADuP9tyjfyCDBhZsK5nWpXMCMVv3LcPr7CpStHOeJyCkrfpcQSYEpVf7GxSU4NrBdu4q-OuHURb9oqbvt3PHMuU1_kO1E142ykrB_d3ZfbkjtkxqUTbRH-aM-QCcO/w640-h326/Elden%20Ring%20Nightreign%20System%20Requirements.png',
+			'https://ik.imagekit.io/2zeqzsn1n/elden-ring.webp?f-webp,q-auto',
 		link: 'https://www.spechype.com/games/elden-ring-nightreign-pc-requirements.html',
 	},
 	{
@@ -70,7 +70,7 @@ export const posts = [
 		description:
 			'Greetings, fellow gamers! Are you prepared to dive straight into Subnautica 2?',
 		image:
-			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjUHyDiiYE5n4oiMa6uZtstLxGfUNGj4KR8zpum4GCcFleCf2mlbxjk02o8OMFH-y1OnpU0UNPedBnRavSC9kin0WbYlMK72dc6diXjmSIYhLq2XqO8ffR-gesQqOTDu2uIw-Dms0DvOVAIKZNcpIIahgDfKgQQsVtqBPLebl7Ws7zh4O88WLPJQy7RUDTW/w640-h360/Subnautica%202%20system%20requirements.png',
+			'https://ik.imagekit.io/2zeqzsn1n/Subnautica-2-system-requirements.webp?f-webp,q-auto',
 		link: 'https://www.spechype.com/games/subnautica-2-requirements.html',
 	},
 	{
@@ -84,7 +84,7 @@ export const posts = [
 		title: 'Once Human System Requirements - Can I Run It?',
 		description: 'In the realm of PC gaming, making sure your system is up to',
 		image:
-			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi3_qXOubI6vqB_Ynfw7tPjafhg7fP_ZKb3xCPhevqd-k1cDNT5XEwJPw2UfBh4PvC9UqFVg74pqFR8cvWQc4aNCuEPQ6PUHbiWFVdkMZ5Aww9AU1F6W1rp60LXswkpRSeWp5gUq4-B1j8rzCof1_BlmY663rGPAyAjC6zmuSwqPGGx_aFyTz_wcOjMugSy/w640-h361/admin-ajax.webp',
+			'https://ik.imagekit.io/2zeqzsn1n/once-human.webp?f-webp,q-auto',
 		link: 'https://www.spechype.com/games/once-human-system-requirements.html',
 	},
 	{
@@ -92,7 +92,7 @@ export const posts = [
 		description:
 			'If you’re a PC gamer who can’t wait to plunge into the stunning world . . .',
 		image:
-			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhM7FmPMO6HpkHog3uRiTWxNtqlziIIbS0M1g25HJOFWnPKUdpDeh-5fBf2rUsLPAbjQBL7AsrztuutnPNUgtvQntqs-5cVI4g0yccWuI7ErsGDsLXpSSWCzTufUdXeQkwj8qFu0KyqVSQYpOOh_xliPFhkfCFUdaYT2CyU5VbzQ3ZC9JFLz_zp5uxMQvmC/w640-h360/Unfdfffddfftitledd-design.webp',
+			'https://ik.imagekit.io/2zeqzsn1n/the-first-descendant.webp?f-webp,q-auto',
 		link: 'https://www.spechype.com/games/the-first-descendant-system-requirements.html',
 	},
 	{
@@ -100,7 +100,7 @@ export const posts = [
 		description:
 			'The Graveyard Shift, a thrilling survival horror game, has captivated players . . .',
 		image:
-			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh0RME1qitoD32dEnp_BmpsaeyGpUfkZM0aj5N0lNBEk5sH3TMp8f0xNz9ZuCvLui6rPoN1IC0s-NJ_VS3w6W45TPuUtFrKDKKJpsCLhiScxLFqKOEDOOlKzBjV8Fi6sKf6e_oFguKJZ64ZOuYV4L0buuZfwdqDeFknSs2dH6cYSI-KhH3xedGxd5dgUACD/w640-h360/wp-17161396491671886920802273103085-1024x576.png',
+			'https://ik.imagekit.io/2zeqzsn1n/the-graveyard-shift.webp?f-webp,q-auto',
 		link: 'https://www.spechype.com/games/the-graveyard-shift-system-requirements.html',
 	},
 ];
