63f386d92871b598703cee4543d8fdd7fce9d573
Roni Sarkar <ronisarkar742202@gmail.com>
2025-05-23 13:52:48 +0530

    add posts and minor fixes

--- js/homePost.js
+++ js/homePost.js
@@ -1,12 +1,20 @@
 export const posts = [
 	
 	{
+		title: 'Clair Obscur: Expedition 33 System Requirements',
+		description:
+			'Discover the official system requirements for Clair Obscur: Expedition 33. Learn the minimum and recommended PC specs to run this turn-based RPG smoothly at 1080p.',
+		image:
+			'https://ik.imagekit.io/2zeqzsn1n/expedition-33.webp?f-webp,q-auto',
+		link: 'https://www.spechype.com/games/clair-obscur-expedition-33-requirements.html',
+	},
+	{
 		title: 'Ark 2 System Requirements - Can I Run It?',
 		description:
 			'Get detailed Ark 2 PC specs, optimization tips, benchmarking data, and upgrade paths for the ultimate dino-slaying experience.',
 		image:
 			'https://ik.imagekit.io/2zeqzsn1n/ark-2.webp?f-webp,q-auto',
-		link: 'games/ark2-system-requirements.html',
+		link: 'https://www.spechype.com/games/ark2-system-requirements.html',
 	},
 	{
 		title: 'Dune: Awakening System Requirements',
@@ -14,7 +22,7 @@ export const posts = [
 			'Check the official system requirements for Dune: Awakening. Find out if your PC can run the game with our detailed guide.',
 		image:
 			'https://ik.imagekit.io/2zeqzsn1n/dune.png?f-webp,q-auto',
-		link: 'games/dune-awakening-system-requirements.html',
+		link: 'https://www.spechype.com/games/dune-awakening-system-requirements.html',
 	},
 	{
 		title: 'Empyreal System Requirements - Can You Run It?',
@@ -22,7 +30,7 @@ export const posts = [
 			'Empyreal is  known for its high-resolution environments, dynamic AI, and complex simulations.',
 		image:
 			'https://ik.imagekit.io/t7v47ftyc/Untitled%20design%20(5).webp?f-webp,q-auto',
-		link: 'games/empyreal-requirements.html',
+		link: 'https://www.spechype.com/games/empyreal-requirements.html',
 	},
 	{
 		title: 'Borderlands 4 Requirements Forecast - Can You Run It?',
@@ -30,7 +38,7 @@ export const posts = [
 			'As anticipation builds for the next installment in the beloved Borderlands franchise.',
 		image:
 			'https://ik.imagekit.io/t7v47ftyc/Borderlands%204%20Requirements%20Forecast%20(1).png?f-webp,q-auto',
-		link: 'games/borderlands-4-requirements.html',
+		link: 'https://www.spechype.com/games/borderlands-4-requirements.html',
 	},
 
 	{
@@ -39,7 +47,7 @@ export const posts = [
 			'Cricket fans, gear up! Cricket 24 delivers stunning visuals and immersive gameplay',
 		image:
 			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiok3RIHSWyQd3rFlXC5-PG3lluq9KwvSyqKr0q_sStZrc_aQ6088wt1P6p-sO_3CGwfMxJYowJa7U_nIn_tmUkudzDgDxTsmuIX6CtH8lDk-4Z06Yce5PLarjN6HADbg8_mmI9iLmIugWPlHaOXArvk-fRDnhtaGTKEOfXgyp8HaP3ePSb594njGBzfcOF/w640-h360/Cricket%2024%20PC%20System%20Requirements.webp',
-		link: 'games/cricket-24-pc-requirements-2025.html',
+		link: 'https://www.spechype.com/games/cricket-24-pc-requirements-2025.html',
 	},
 
 	{
@@ -47,7 +55,7 @@ export const posts = [
 		description: 'Hyper Light Breaker" is an upcoming roguelite action game.',
 		image:
 			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgaWS9FhciBjTyzDEE2zOs2_inEbk5EuD1EYrZmanqyiOrWblFSA0WixGH1aeWfz_O1TOJMhWDi5uRvGj9PeUkPoOuj0bt5u6HxGbMOSTAEp0URY9l1Y_eQpc5ZhUOXP2uQYpVR8Vvc-npq7YbiPZY0lofuXq-P6cSE8-Gk4nBK_valfeqHNxpSR5S-YlF6/w640-h360/Hyper%20Light%20Breaker%20System%20Requirements%20.png',
-		link: 'games/hyper-light-breaker-requirements.html',
+		link: 'https://www.spechype.com/games/hyper-light-breaker-requirements.html',
 	},
 	{
 		title: 'Elden Ring Nightreign System Requirements - Can i run it?',
@@ -55,7 +63,7 @@ export const posts = [
 			'Check the full PC system requirements for Elden Ring: Nightreign, including minimum and recommended specs, 4K gaming builds, storage tips, and co-op settings.',
 		image:
 			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgn6xCE7y45BPdP2mX3hLqRfg5jgJu5FGdOBx2yzC0Meoznlv_4hRANvVrvm7tsqdDADuP9tyjfyCDBhZsK5nWpXMCMVv3LcPr7CpStHOeJyCkrfpcQSYEpVf7GxSU4NrBdu4q-OuHURb9oqbvt3PHMuU1_kO1E142ykrB_d3ZfbkjtkxqUTbRH-aM-QCcO/w640-h326/Elden%20Ring%20Nightreign%20System%20Requirements.png',
-		link: 'games/elden-ring-nightreign-pc-requirements.html',
+		link: 'https://www.spechype.com/games/elden-ring-nightreign-pc-requirements.html',
 	},
 	{
 		title: 'Subnautica 2 system requirements',
@@ -63,21 +71,21 @@ export const posts = [
 			'Greetings, fellow gamers! Are you prepared to dive straight into Subnautica 2?',
 		image:
 			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjUHyDiiYE5n4oiMa6uZtstLxGfUNGj4KR8zpum4GCcFleCf2mlbxjk02o8OMFH-y1OnpU0UNPedBnRavSC9kin0WbYlMK72dc6diXjmSIYhLq2XqO8ffR-gesQqOTDu2uIw-Dms0DvOVAIKZNcpIIahgDfKgQQsVtqBPLebl7Ws7zh4O88WLPJQy7RUDTW/w640-h360/Subnautica%202%20system%20requirements.png',
-		link: 'games/subnautica-2-requirements.html',
+		link: 'https://www.spechype.com/games/subnautica-2-requirements.html',
 	},
 	{
 		title: 'Destiny 2 System Requirements',
 		description:
 			'Destiny 2 is an epic online-only multiplayer first-person shooter . . .',
 		image: 'https://ik.imagekit.io/t7v47ftyc/Untitled%20design.png?f-webp,q-auto',
-		link: 'games/destiny-2-requirements.html',
+		link: 'https://www.spechype.com/games/destiny-2-requirements.html',
 	},
 	{
 		title: 'Once Human System Requirements - Can I Run It?',
 		description: 'In the realm of PC gaming, making sure your system is up to',
 		image:
 			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi3_qXOubI6vqB_Ynfw7tPjafhg7fP_ZKb3xCPhevqd-k1cDNT5XEwJPw2UfBh4PvC9UqFVg74pqFR8cvWQc4aNCuEPQ6PUHbiWFVdkMZ5Aww9AU1F6W1rp60LXswkpRSeWp5gUq4-B1j8rzCof1_BlmY663rGPAyAjC6zmuSwqPGGx_aFyTz_wcOjMugSy/w640-h361/admin-ajax.webp',
-		link: 'games/once-human-system-requirements.html',
+		link: 'https://www.spechype.com/games/once-human-system-requirements.html',
 	},
 	{
 		title: 'The First Descendant system requirements',
@@ -85,7 +93,7 @@ export const posts = [
 			'If you’re a PC gamer who can’t wait to plunge into the stunning world . . .',
 		image:
 			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhM7FmPMO6HpkHog3uRiTWxNtqlziIIbS0M1g25HJOFWnPKUdpDeh-5fBf2rUsLPAbjQBL7AsrztuutnPNUgtvQntqs-5cVI4g0yccWuI7ErsGDsLXpSSWCzTufUdXeQkwj8qFu0KyqVSQYpOOh_xliPFhkfCFUdaYT2CyU5VbzQ3ZC9JFLz_zp5uxMQvmC/w640-h360/Unfdfffddfftitledd-design.webp',
-		link: 'games/the-first-descendant-system-requirements.html',
+		link: 'https://www.spechype.com/games/the-first-descendant-system-requirements.html',
 	},
 	{
 		title: 'The Graveyard Shift System Requirements',
@@ -93,17 +101,17 @@ export const posts = [
 			'The Graveyard Shift, a thrilling survival horror game, has captivated players . . .',
 		image:
 			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh0RME1qitoD32dEnp_BmpsaeyGpUfkZM0aj5N0lNBEk5sH3TMp8f0xNz9ZuCvLui6rPoN1IC0s-NJ_VS3w6W45TPuUtFrKDKKJpsCLhiScxLFqKOEDOOlKzBjV8Fi6sKf6e_oFguKJZ64ZOuYV4L0buuZfwdqDeFknSs2dH6cYSI-KhH3xedGxd5dgUACD/w640-h360/wp-17161396491671886920802273103085-1024x576.png',
-		link: 'games/the-graveyard-shift-system-requirements.html',
+		link: 'https://www.spechype.com/games/the-graveyard-shift-system-requirements.html',
 	},
 ];
 
 export const popularPosts = [
 	{
-		title1: 'Elden Ring Nightreign System Requirements',
+		title1: 'Clair Obscur: Expedition 33 System Requirements',
 		link1:
-			'https://www.spechype.com/games/elden-ring-nightreign-pc-requirements.html',
+			'https://www.spechype.com/games/clair-obscur-expedition-33-requirements.html',
 		image1:
-			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgn6xCE7y45BPdP2mX3hLqRfg5jgJu5FGdOBx2yzC0Meoznlv_4hRANvVrvm7tsqdDADuP9tyjfyCDBhZsK5nWpXMCMVv3LcPr7CpStHOeJyCkrfpcQSYEpVf7GxSU4NrBdu4q-OuHURb9oqbvt3PHMuU1_kO1E142ykrB_d3ZfbkjtkxqUTbRH-aM-QCcO/w640-h326/Elden%20Ring%20Nightreign%20System%20Requirements.png',
+			'https://ik.imagekit.io/2zeqzsn1n/expedition-33.webp?f-webp,q-auto',
 
 		title2: 'Borderlands 4 Requirements',
 		link2:
@@ -122,47 +130,5 @@ export const popularPosts = [
 			'https://www.spechype.com/games/destiny-2-requirements.html',
 		image4:
 			'https://ik.imagekit.io/t7v47ftyc/Untitled%20design.png?f-webp,q-auto',
-	},
-];
-
-export const mightLike = [
-	{
-		cards: [
-			{
-				image: 'https://ik.imagekit.io/t7v47ftyc/Untitled%20design.png?f-webp,q-auto',
-				title: 'Destiny 2 System Requirements',
-				link: 'https://www.spechype.com/games/destiny-2-requirements.html',
-			},
-			{
-				image:
-					'https://ik.imagekit.io/t7v47ftyc/Borderlands%204%20Requirements%20Forecast%20(1).png?f-webp,q-auto',
-				title: 'Borderlands 4 Requirements Forecast for PC',
-				link: 'https://www.spechype.com/games/borderlands-4-requirements.html',
-			},
-			{
-				image:
-					'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiok3RIHSWyQd3rFlXC5-PG3lluq9KwvSyqKr0q_sStZrc_aQ6088wt1P6p-sO_3CGwfMxJYowJa7U_nIn_tmUkudzDgDxTsmuIX6CtH8lDk-4Z06Yce5PLarjN6HADbg8_mmI9iLmIugWPlHaOXArvk-fRDnhtaGTKEOfXgyp8HaP3ePSb594njGBzfcOF/w640-h360/Cricket%2024%20PC%20System%20Requirements.webp',
-				title: 'Cricket 24 PC System Requirements 2025',
-				link: 'https://www.spechype.com/games/cricket-24-pc-requirements-2025.html',
-			},
-			{
-				image:
-					'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgaWS9FhciBjTyzDEE2zOs2_inEbk5EuD1EYrZmanqyiOrWblFSA0WixGH1aeWfz_O1TOJMhWDi5uRvGj9PeUkPoOuj0bt5u6HxGbMOSTAEp0URY9l1Y_eQpc5ZhUOXP2uQYpVR8Vvc-npq7YbiPZY0lofuXq-P6cSE8-Gk4nBK_valfeqHNxpSR5S-YlF6/w640-h360/Hyper%20Light%20Breaker%20System%20Requirements%20.png',
-				title: 'Hyper Light Breaker System Requirements',
-				link: 'https://www.spechype.com/games/hyper-light-breaker-requirements.html',
-			},
-			{
-				image:
-					'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgn6xCE7y45BPdP2mX3hLqRfg5jgJu5FGdOBx2yzC0Meoznlv_4hRANvVrvm7tsqdDADuP9tyjfyCDBhZsK5nWpXMCMVv3LcPr7CpStHOeJyCkrfpcQSYEpVf7GxSU4NrBdu4q-OuHURb9oqbvt3PHMuU1_kO1E142ykrB_d3ZfbkjtkxqUTbRH-aM-QCcO/w640-h326/Elden%20Ring%20Nightreign%20System%20Requirements.png',
-				title: 'Elden Ring Nightreign System Requirements',
-				link: 'https://www.spechype.com/games/elden-ring-nightreign-pc-requirements.html',
-			},
-			{
-				image:
-					'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjUHyDiiYE5n4oiMa6uZtstLxGfUNGj4KR8zpum4GCcFleCf2mlbxjk02o8OMFH-y1OnpU0UNPedBnRavSC9kin0WbYlMK72dc6diXjmSIYhLq2XqO8ffR-gesQqOTDu2uIw-Dms0DvOVAIKZNcpIIahgDfKgQQsVtqBPLebl7Ws7zh4O88WLPJQy7RUDTW/w640-h360/Subnautica%202%20system%20requirements.png',
-				title: 'Subnautica 2 system requirements',
-				link: 'https://www.spechype.com/games/subnautica-2-requirements.html',
-			},
-		],
 	},
 ];
--- js/recomend.js
+++ js/recomend.js
@@ -1,39 +1,36 @@
-import { mightLike } from './homePost.js';
+import { posts } from './homePost.js';
 
+let postsHTML = '';
 
+// Shuffle posts array and pick first 6
+const shuffledPosts = posts.sort(() => 0.5 - Math.random()).slice(0, 6);
 
-let cardsHTML = '';
-
-mightLike.forEach((section) => {
-	section.cards.forEach((card) => {
-		cardsHTML += `
-      <div class="bg-white rounded-lg overflow-hidden transition p-2 group">
-  <div class="overflow-hidden rounded-lg">
-    <a href="${card.link}">
-      <img src="${card.image}" alt="Game Image"
-           class="w-full h-auto object-cover rounded-lg transform transition-transform duration-300 group-hover:scale-105">
-    </a>
-  </div>
-  <div class="mt-2">
-    <span class="text-xs text-white bg-[#0E579F] px-2 py-1 rounded">GAMES</span>
-    <a class="font-semibold mt-2 text-gray-800 text-sm flex leading-snug" href="${card.link}">
-      ${card.title}
-    </a>
-  </div>
-</div>
-
-
-    `;
-	});
+shuffledPosts.forEach((post) => {
+	postsHTML += `
+    <div class="bg-white rounded-lg overflow-hidden transition p-2 group">
+      <div class="overflow-hidden rounded-lg">
+        <a href="${post.link}">
+          <img src="${post.image}" alt="Game Image"
+               class="w-full h-auto object-cover rounded-lg transform transition-transform duration-300 group-hover:scale-105">
+        </a>
+      </div>
+      <div class="mt-2">
+        <span class="text-xs text-white bg-[#0E579F] px-2 py-1 rounded">GAMES</span>
+        <a class="font-semibold mt-2 text-gray-800 text-sm flex leading-snug" href="${post.link}">
+          ${post.title}
+        </a>
+      </div>
+    </div>
+  `;
 });
 
 const sectionHTML = `
-  <div class="bg-white w-fit max-w-full sm:max-w-6xl  rounded-lg shadow p-4 sm:p-6 mb-4  mx-auto mt-28 flex flex-col px-4  sm:mt-4">
+  <div class="bg-white w-fit max-w-full sm:max-w-6xl rounded-lg shadow p-4 sm:p-6 mb-4 mx-auto mt-28 flex flex-col px-4 sm:mt-4">
     <h2 class="text-lg font-semibold text-gray-800 border-b-2 border-[#339af0] inline-block mb-4">
       YOU MIGHT LIKE
     </h2>
     <div class="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 gap-3 sm:gap-4 md:gap-6">
-      ${cardsHTML}
+      ${postsHTML}
     </div>
   </div>
 `;
