92ceb556bba1a4e66dbce7b7af3ab7eb9c209e53
Roni Sarkar <ronisarkar742202@gmail.com>
2025-05-24 13:54:16 +0530

    2

--- js/addmain.js
+++ js/addmain.js
@@ -63,7 +63,7 @@ const headerHTML = `
 // Insert the navbar at the top of the <body>
 document.body.insertAdjacentHTML('afterbegin', headerHTML);
 
-const sidebarList = document.querySelector('#sidebar');
+const sidebarList = document.querySelector('#sidebar div');
 
 popularPosts.forEach((item) => {
 	sidebarList.innerHTML += `
